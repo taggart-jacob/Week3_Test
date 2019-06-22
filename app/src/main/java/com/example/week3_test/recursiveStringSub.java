f7e4ee6215d7f2556e2970566ea5dbfe834940b0
Jacob Taggart <jacob.taggart@mobileappscompany.us>
2019-06-21 23:28:21 -0400

    I'm an idiot.

--- app/src/main/java/com/example/week3_test/recursiveStringSub.java
+++ app/src/main/java/com/example/week3_test/recursiveStringSub.java
@@ -7,32 +7,22 @@ public class recursiveStringSub {
     static int count = 0;
 
     public static void main(String[] args) {
-        System.out.print(copySubs("catcowcat", "cat", 2));
+        System.out.print(copySubs("cacowadcat", "cat", 2));
     }
 
     private static boolean copySubs(String catcowcat, String string, int num) {
-        int length1 = catcowcat.length();
-        int length2 = string.length();
 
+        if (catcowcat.contains(string)){
+            count++;
+            String newString = catcowcat.replaceFirst(string, string.substring(0, 1));
 
-        if (length1 == 0 || length1 < length2) {
+            return copySubs(newString, string, num);
+        }
+        else if (num == count){
+            return true;
+        }
+        else{
             return false;
         }
-
-        if (catcowcat.substring(0, length2).equals(string)) {
-            count += 1;
-            if (num == count) {
-                return true;
-            } else
-                return copySubs(catcowcat.substring(length2 - 1),
-                        string, num);
-        }
-        if (catcowcat.substring(length2 - 1).equals(string)) {
-            if (num == count) {
-                return true;
-            }
-        }
-        return false;
-
     }
 }
